c013612f43b20098ddd0e80b5c1672dd93849398
Blake Godsey <somethingclever182@gmail.com>
2025-04-15 18:47:45 -0500

    added a call to the function and changed the totalPrice calc to start at 0

--- discountCalculator.js
+++ discountCalculator.js
@@ -1,17 +1,24 @@
+debugger;
+
+
 function calculateDiscountedPrice(quantity, pricePerItem) {
-    let totalPrice = 0;
     debugger
-    for (let i = 1; i < quantity; i++) {
+    let totalPrice = 0;
+    
+    for (let i = 0; i < quantity; i++) {
+        debugger
         totalPrice += pricePerItem;
     }
-
+    
     if (quantity >= 10) {
+        debugger
         totalPrice *= 0.9;
     }
-
+   
     return totalPrice;
 }
 
+calculateDiscountedPrice(10, 1000); // Example usage: 10 items at $1000 each
 
 
 module.exports = calculateDiscountedPrice;
